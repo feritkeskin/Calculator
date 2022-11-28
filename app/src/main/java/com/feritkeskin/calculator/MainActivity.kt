6e335aa6eb81ff13416d47aac57065d10ed0d5b8
feritkeskin <ferit.keskin69@gmail.com>
2022-11-28 03:40:53 +0300

    Update.

--- app/src/main/java/com/feritkeskin/calculator/MainActivity.kt
+++ app/src/main/java/com/feritkeskin/calculator/MainActivity.kt
@@ -2,140 +2,81 @@ package com.feritkeskin.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-
-    var isNewOp = true
-    var oldNumber = ""
-    var op = "+"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun oncBtn(view: View) {
+        //Number listeners
+        btn00.setOnClickListener { appendOnClick(true, "00") }
+        btn0.setOnClickListener { appendOnClick(true, "0") }
+        btn1.setOnClickListener { appendOnClick(true, "1") }
+        btn2.setOnClickListener { appendOnClick(true, "2") }
+        btn3.setOnClickListener { appendOnClick(true, "3") }
+        btn4.setOnClickListener { appendOnClick(true, "4") }
+        btn5.setOnClickListener { appendOnClick(true, "5") }
+        btn6.setOnClickListener { appendOnClick(true, "6") }
+        btn7.setOnClickListener { appendOnClick(true, "7") }
+        btn8.setOnClickListener { appendOnClick(true, "8") }
+        btn9.setOnClickListener { appendOnClick(true, "9") }
+        btnDot.setOnClickListener { appendOnClick(true, ".") }
 
-        if (isNewOp)
-            edit_text.setText("")
-        isNewOp = false
+        //Operator Listeners
+        btnPlus.setOnClickListener { appendOnClick(false, "+") }
+        btnMinus.setOnClickListener { appendOnClick(false, "-") }
+        btnMultiply.setOnClickListener { appendOnClick(false, "*") }
+        btnDivide.setOnClickListener { appendOnClick(false, "/") }
+        btnLeftB.setOnClickListener { appendOnClick(false, "(") }
+        btnRightB.setOnClickListener { appendOnClick(false, ")") }
 
-        val btnSelect = view as Button
-        var btnClicked: String = edit_text.text.toString()
 
-        when (btnSelect.id) {
-
-            button_0.id -> {
-                btnClicked += "0"
-            }
-
-            button_1.id -> {
-                btnClicked += "1"
-            }
-
-            button_2.id -> {
-                btnClicked += "2"
-            }
-
-            button_3.id -> {
-                btnClicked += "3"
-            }
-
-            button_4.id -> {
-                btnClicked += "4"
-            }
-
-            button_5.id -> {
-                btnClicked += "5"
-            }
-
-            button_6.id -> {
-                btnClicked += "6"
-            }
-
-            button_7.id -> {
-                btnClicked += "7"
-            }
-
-            button_8.id -> {
-                btnClicked += "8"
-            }
-
-            button_9.id -> {
-                btnClicked += "9"
-            }
-
-            button_nokta.id -> {
-                btnClicked += "."
-            }
-
-            button_artiEksi.id -> {
-                btnClicked = "-$btnClicked"
-            }
+        btnClear.setOnClickListener {
+            clear()
         }
-        edit_text.setText(btnClicked)
 
-    }
-
-    fun oncOp(view: View) {
-
-        isNewOp = true
-        oldNumber = edit_text.text.toString()
-        val btnSelect: Button = view as Button
-
-        when (btnSelect.id) {
-            button_bol.id -> {
-                op = "/"
-            }
-            button_carp.id -> {
-                op = "*"
-            }
-            button_cikar.id -> {
-                op = "-"
-            }
-            button_topla.id -> {
-                op = "+"
-            }
+        btnEqual.setOnClickListener {
+            calculate()
+            tvInput.text = ""
         }
     }
 
-    fun buttonEsittir(view: View) {
-        if (edit_text.text == null) {
-            val newNumber = edit_text.text.toString()
-            var result = 0
+    private fun appendOnClick(clear: Boolean, string: String) {
 
-            when (op) {
-
-                "/" -> {
-                    result = oldNumber.toInt() / newNumber.toInt()
-                }
-                "*" -> {
-                    result = oldNumber.toInt() * newNumber.toInt()
-                }
-                "-" -> {
-                    result = oldNumber.toInt() - newNumber.toInt()
-                }
-                "+" -> {
-                    result = oldNumber.toInt() + newNumber.toInt()
-                }
-            }
-            edit_text.setText(result.toString())
+        if (clear) {
+            tvOutput.text = ""
+            tvInput.append(string)
+        } else {
+            tvInput.append(tvOutput.text)
+            tvInput.append(string)
+            tvOutput.text = ""
         }
     }
 
-    fun delete(view: View) {
-        edit_text.setText("0")
-        isNewOp = true
+    private fun clear() {
+        tvInput.text = ""
+        tvOutput.text = ""
     }
 
-    fun yuzde(view: View) {
-        if (edit_text.text == null){
-            val no: Double = edit_text.text.toString().toDouble() / 100
-            edit_text.setText(no.toString())
-            isNewOp = true
+    private fun calculate() {
+
+        try {
+            val input = ExpressionBuilder(tvInput.text.toString()).build()
+            val output = input.evaluate()
+            val longOutput = output.toLong()
+            if (output == longOutput.toDouble()) {
+                tvOutput.text = longOutput.toString()
+            } else {
+                tvOutput.text = output.toString()
+            }
+
+        } catch (e: Exception) {
+            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
         }
     }
 }
+
